c35938f3da90773159d4dcc6f80343916e112aa6
Naina <ns1388@scarletmail.rutgers.edu>
2023-08-01 11:41:05 -0400

    Initial Commit

---
+++ TheWarGame/TheWarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheWarGameApp.swift
+//  TheWarGame
+//
+//  Created by Nayana Sharma on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TheWarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
